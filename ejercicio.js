6555a64f6e3569064226614343cc07a402ad2950
AnaaCF <anagisel.castillo@gmail.com>
2023-04-21 16:10:19 -0300

    modificacion ejercicio 3

--- ejercicio.js
+++ ejercicio.js
@@ -1,28 +1,28 @@
-function titulo(nroEjercicio,nombreEjercicio){
+/*function titulo(nroEjercicio,nombreEjercicio){
     alert("EJERCICIO " + nroEjercicio +  " --> " + nombreEjercicio)
     document.write("<br>"); 
     document.write("<h2>EJERCICIO "+ nroEjercicio+"<h2>" );     
-}
-
+}*/
+const titulo = (nroEjercicio,nombreEjercicio) =>console.log(`EJERCICIO ${nroEjercicio}--> ${nombreEjercicio}<br>`)
+    
 //EJERCICIO 1
 
-    function piramide(nro){
-        for(i=1;i<=nro;i++){
-            for(j=1;j<=i;j++){
-                document.write(j);
-            }
-            document.write("<br>");
-        }    
-    }
-    let nro= parseInt(prompt("ingrese un nro para la pirámide")); 
-    piramide(nro);
-    console.log("Comenzar ejercicio 1");
-
+titulo(1,"PIRAMIDE");
+function piramide(nro){
+    for(i=1;i<=nro;i++){
+        for(j=1;j<=i;j++){
+            document.write(j);           
+        }
+        document.write("<br>");        
+    }    
+}
+let nro= parseInt(prompt("ingrese un nro para la pirámide")); 
+piramide(nro);
+console.log("Comenzar ejercicio 1")
     
-
 //EJERCICIO2
 
-titulo(2,"ARRAYS")
+titulo(2,"ARRAYS");
 
     function llenarArrays(nuevo){
         alert("LLENAREMOS UN ARRAY");
@@ -32,7 +32,7 @@ titulo(2,"ARRAYS")
            nuevo.push(valor);
         }
     }
-    function obtenerLargo(uno,dos){
+    function obtenerMasLargo(uno,dos){
         if(uno.length<dos.length){
             return array1.length;
         }else{
@@ -40,85 +40,66 @@ titulo(2,"ARRAYS")
         }  
     }
     function compararArrays(uno,dos){
-        nro = obtenerLargo(uno,dos);
+        nro = obtenerMasLargo(uno,dos);
         let arrayIguales=[];
-        for(i=0;i<=nro;i++){
-            valor=uno[i];
-            for(j=0;j<=nro;j++){
-                if(valor===dos[j]){
-                    arrayIguales.push(valor);
-                }
-            }
+        for(i=0;i< nro;i++){
+           if(uno.includes(dos[i])){
+                arrayIguales.push(dos[i]);
+           }
         }
-        return arrayIguales;
+        console.log(arrayIguales);
+        return arrayIguales;        
     }
     
     let array1 = [];
     let array2 =[];
-    let nuevo = [];
-   
+    let nuevo = [];   
  
     llenarArrays(array1);
-    llenarArrays(array2);
-    
+    llenarArrays(array2);    
     nuevo = compararArrays(array1,array2);
-    console.log("Array con los valores iguales es -->  "+nuevo);
+
     document.write("Primer Array ingresado --> "+array1+"<br>");
     document.write("Segundo Array ingresado --> "+array2+"<br>");
     document.write("Array con los valores iguales es -->  "+nuevo);
 
+
 //EJERCICIO 3
 titulo(3,"CLASES") ;
     class Carrito {
-        montoTotal;
         productos = [];
         unidades = [];
-        constructor(montoTotal,productos,unidades){
-            this.montoTotal = montoTotal;
-            this.productos = [productos];
-            this.unidades = [unidades];
-        }
-     
-        agregarProducto(nombre, precio, unidades){
+        montoTotal;
+    
+        constructor(productos,montoTotal,unidades,){           
+            this.productos = [];
+            this.montoTotal = 0;
+            this.unidades = [];           
+        }   
+
+        agregarProducto(nombre, precio, unidades){           
             
-            this.productos.push(nombre);
-            this.unidades.push(unidades);
-            this.montoTotal=this.montoTotal+(precio*unidades);
+            let condicion = true;
+            for(let i=0;i<this.productos.length;i++){
+                if(nombre==this.productos[i]){
+                    console.log(`ya existe el producto ${nombre} con ${this.unidades[i]} unidades`);
+                    condicion = false;
+                }
+            }
+            if (condicion==true){
+               this.productos.push(nombre);
+               this.unidades.push(unidades);
+               this.montoTotal =+ (precio*unidades);
+                  
+            }           
             
-        }
+        }   
       
     } 
-    
-    function comparar(objeto,nombreProducto, precioProducto, unidProducto){
-        let condicion = true;
-        for(i=0;i<objeto.productos.length;i++){
-            if(nombreProducto==objeto.productos[i]){
-                console.log("ya existe el producto "+ nombreProducto + " con "+objeto.unidades[i]+" unidades");
-                document.write("<br>ya existe el producto "+ nombreProducto + " con "+objeto.unidades[i]+" unidades<br>");
-                condicion = false;
-            }
-        }
-        if (condicion==true){
-            carrito1.agregarProducto(nombreProducto, precioProducto, unidProducto);
-        }
-    }
-    function mostrarCarrito(objeto){
-        document.write("-----<br>Contenido del Carrito de compras<br>");
-        for(i=0;i<objeto.productos.length;i++){
-            document.write("producto "+objeto.productos[i]+" con "+objeto.unidades[i]+" unidades <br>");
-        }
-        document.write("Monto total "+objeto.montoTotal+"<br>-----");
-    }
  
-    const carrito1 = new Carrito(200,"azucar",2);   
-    mostrarCarrito(carrito1);
-    carrito1.agregarProducto("leche",400,2);
-    comparar(carrito1,"azucar",200,3);
-    console.log(carrito1);
-    comparar(carrito1,"yerba",500,1); 
-    mostrarCarrito(carrito1);
-
-
+    const carrito1 = new Carrito;  
+   
+     
 
 
 
